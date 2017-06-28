101faff334b7774d03503995a3a126fae62e95e4
apardo <apardo@corunet.com>
2017-06-28 16:05:56 +0200

    Map and Filter

--- github-battle/app/index.js
+++ github-battle/app/index.js
@@ -9,18 +9,44 @@ require('./index.css');
 //npm install --save-dev babel-core babel-loader babel-preset-env babel-preset-react css-loader style-loader html-webpack-plugin webpack webpack-dev-server
 
 
-class App extends React.Component {
-    render(){
-        return (
-            <div>
-                Hello React Training!
-            </div>
-        )
-    }
+class Users extends React.Component {
+  render() {
+    var friends = this.props.list.filter(function (person){
+      return person.friend === true;
+    });
+    var nonFriends = this.props.list.filter(function (person){
+      return person.friend !== true;
+    });
+    return (
+      <div>
+        <h1>Friends</h1>
+        <ul>
+          {friends.map(function (person, index){
+            return <li key={index}>{person.name}</li>
+          })}
+        </ul>
+        
+        <hr />
+        
+        <h1> Non Friends </h1>
+        <ul>
+          {nonFriends.map(function (person, index){
+            return <li key={index}>{person.name}</li>
+          })}
+        </ul>        
+      </div>
+    )
+  }
 }
 
 ReactDOM.render(
-    <App />,
-    document.getElementById('app')
+  <Users list={[
+    { name: 'Tyler', friend: true },
+    { name: 'Ryan', friend: true },
+    { name: 'Michael', friend: false },
+    { name: 'Mikenzi', friend: false },
+    { name: 'Jessica', friend: true },
+    { name: 'Dan', friend: false } ]} 
+  />,
+  document.getElementById('app')
 );
-
