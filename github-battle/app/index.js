4458db9db29a76d9f4e3c9519bf32ce81d08c593
apardo <apardo@corunet.com>
2017-06-29 18:34:12 +0200

    Added components folder and popular component

--- github-battle/app/index.js
+++ github-battle/app/index.js
@@ -1,60 +1,15 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 require('./index.css');
-var PropTypes = require('prop-types');
 
+var App = require('./components/App');
 // state
 // lifecycle event
 // UI
 
 //npm install --save-dev babel-core babel-loader babel-preset-env babel-preset-react css-loader style-loader html-webpack-plugin webpack webpack-dev-server
 
-
-class Users extends React.Component {
-  render() {
-    var friends = this.props.list.filter(function (person){
-      return person.friend === true;
-    });
-    var nonFriends = this.props.list.filter(function (person){
-      return person.friend !== true;
-    });
-    return (
-      <div>
-        <h1>Friends</h1>
-        <ul>
-          {friends.map(function (person, index){
-            return <li key={index}>{person.name}</li>
-          })}
-        </ul>
-        
-        <hr />
-        
-        <h1> Non Friends </h1>
-        <ul>
-          {nonFriends.map(function (person, index){
-            return <li key={index}>{person.name}</li>
-          })}
-        </ul>        
-      </div>
-    )
-  }
-}
-Users.propTypes = {
-    list: PropTypes.arrayOf(PropTypes.shape({
-        name: PropTypes.string.isRequired,
-        friend: PropTypes.bool.isRequired
-    }))
-}
-
-
 ReactDOM.render(
-  <Users list={[
-    { name: 'Tyler', friend: true },
-    { name: 'Ryan', friend: true },
-    { name: 'Michael', friend: false },
-    { name: 'Mikenzi', friend: false },
-    { name: 'Jessica', friend: true },
-    { name: 'Dan', friend: false } ]} 
-  />,
+  <App />,
   document.getElementById('app')
 );
